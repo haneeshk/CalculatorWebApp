8e88d8ad3261c2bfe912202bc697ba457fb5c0de
Haneesh Kesari <haneesh_kesari@brown.edu>
2024-01-01 16:57:01 -0500

    compiled gsl library using emscripten

--- public/javascript/addDoublesEmscripten.c
+++ public/javascript/addDoublesEmscripten.c
@@ -1,10 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <gsl/gsl_sf_bessel.h>
 
-double addDoublesEmscripten(double input) {
+
+double addDoublesEmscripten(double input1, double input2) {
     
     
-    return input*2.0;
+    return input1+input2;
 }
 
+
+
+double ComputeBesselJ0 (double x)
+{
+  
+   
+  
+  return gsl_sf_bessel_J0(x);
+}
--- public/myScript.js
+++ public/myScript.js
@@ -5,17 +5,22 @@ Module.onRuntimeInitialized = function() {
         event.preventDefault();
 
         // Get the input value
-        var inputValue = document.getElementById('myInput').value;
+        var inputValue1 = document.getElementById('myInput1').value;
+        var inputValue2 = document.getElementById('myInput2').value;
+        var inputValue3 = document.getElementById('myInput3').value;
 
         // Allocate memory for the string
         
         
         var myFunction = Module.cwrap('addDoublesEmscripten', 'number', ['number', 'number']);
+        var ComputeBesselJ0 = Module.cwrap('ComputeBesselJ0', 'number', ['number']);
         // Call a function from your C/C++ code
-        var result = myFunction(inputValue, inputValue);
+        var result1 = myFunction(inputValue1, inputValue2);
+        var result2 = ComputeBesselJ0(inputValue3);
 
         // Deallocate memory
-        document.getElementById('output2').textContent = result;  
+        document.getElementById('output2').textContent = result1;
+        document.getElementById('output3').textContent = result2;  
 
         // Display the result
       console.log(result);  
