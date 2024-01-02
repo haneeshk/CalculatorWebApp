62219c1c1b0acfe0168c6edd8246cef91115c91c
Haneesh Kesari <haneesh_kesari@brown.edu>
2024-01-01 20:51:13 -0500

    added multiple pages

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
 const express = require('express');
 const { exec } = require('child_process');
-// const path = require('path');
+const path = require('path');
 const bodyParser = require('body-parser');
 const app = express();
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
+    console.log(path.join(__dirname, 'public', 'index.html'));
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    console.log(path.join(__dirname, 'public', 'index.html'));
+    
+    
 });
 
+
+app.get('/about', (req, res) => {
+    res.sendFile(path.join(__dirname,'public/pages','background.html'));
+    //  res.send('This is the about ');
+});
 // const addDoubles = require('./addDoubles.js');
 
 app.post('/run', (req, res) => {
